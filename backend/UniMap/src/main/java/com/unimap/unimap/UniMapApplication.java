4ca3ee5787db2655c8e0a524f87b353ad05c726a
Salem <id.salemali@gmail.com>
2025-10-09 21:31:07 +0200

    The very first commit

---
+++ backend/UniMap/src/main/java/com/unimap/unimap/UniMapApplication.java
@@ -0,0 +1,13 @@
+package com.unimap.unimap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniMapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniMapApplication.class, args);
+    }
+
+}
---
+++ backend/UniMap/src/test/java/com/unimap/unimap/UniMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unimap.unimap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniMapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
