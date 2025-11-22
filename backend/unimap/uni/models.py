44ddef65b44f5d63558dd62ce540d933de6ea9f0
Salem <id.salemali@gmail.com>
2025-11-22 11:20:23 +0100

    added db tables for uni

--- backend/unimap/uni/models.py
+++ backend/unimap/uni/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+class Campus(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    address = models.CharField(max_length=200, null=False, blank=False)
+    maps_url = models.URLField(null=False, blank=False)
+    image = models.ImageField(upload_to='images/campuses')
+
+    class Meta:
+        ordering = ['name']
+
+class Building(models.Model):
+    name = models.CharField(max_length=100, null=False, blank=False)
+    campus = models.ForeignKey(Campus, related_name='buildings')
+    address = models.CharField(max_length=200, null=False, blank=False)
+    maps_url = models.URLField(null=False, blank=False)
+
+    class Meta:
+        ordering = ['name']
+
+class Room(models.Model):
+    name = models.CharField(max_length=50)
+    type = models.CharField(max_length=50)
+    floor = models.SmallIntegerField()
+    building = models.ForeignKey(Building, related_name='rooms')
+
+    class Meta:
+        ordering = ['floor', 'name']
