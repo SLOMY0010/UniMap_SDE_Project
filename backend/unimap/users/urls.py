103ab3ca98324f754961d9d104ac557c9de86d72
Salem <id.salemali@gmail.com>
2025-11-24 11:30:27 +0100

    fixed and added api_routes.txt

--- backend/unimap/users/urls.py
+++ backend/unimap/users/urls.py
@@ -4,4 +4,12 @@ from .views import *
 urlpatterns = [
     path('users/', UserView.as_view()),
     path('users/<int:pk>/', UserView.as_view()),
+
+    # For Authentication
+    path('register/', RegisterView.as_view()),
+    path('login/', LoginView.as_view()),
+    path('logout/', LogoutView.as_view()),
+    path('chk-tkn/', TokenRemainingTimeView.as_view()),
+
+    
 ]
