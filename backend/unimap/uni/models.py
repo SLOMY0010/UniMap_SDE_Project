483de3cb46fcdea34c80b7199c3d47e45a6d5f80
Starcouch <anandprasad.mathew@gmail.com>
2025-11-29 15:27:05 +0100

    updated models class and added the json file for data

--- backend/unimap/uni/models.py
+++ backend/unimap/uni/models.py
@@ -18,7 +18,6 @@ class Building(models.Model):
     campus = models.ForeignKey(Campus, related_name='buildings', on_delete=models.CASCADE)
     address = models.CharField(max_length=200, null=False, blank=False)
     maps_url = models.URLField(null=False, blank=False)
-    floors_num = models.PositiveIntegerField()
 
     class Meta:
         ordering = ['name']
@@ -34,17 +33,45 @@ ROOM_TYPES = {
     "male washroom": "Male Washroom",
     "cafeteria": "Cafeteria"
 }
-    
-
 
 class Room(models.Model):
     name = models.CharField(max_length=50)
     type = models.CharField(max_length=50, choices=ROOM_TYPES)
-    floor = models.SmallIntegerField()
+    floor_map = models.ForeignKey('FloorMap', on_delete=models.CASCADE, related_name='rooms', blank=True, null=True)
     building = models.ForeignKey(Building, related_name='rooms', on_delete=models.CASCADE)
 
     class Meta:
-        ordering = ['floor', 'name']
+        ordering = ['name']
 
     def __str__(self):
-        return f"Room: {self.name}, Floor: {self.floor} "
+        return f"Room: {self.name}"
+    
+class FloorMap(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='floors', null=True, blank=True)
+    floor_number = models.IntegerField()
+    image = models.ImageField(upload_to="images/floor_maps/")
+
+
+class CampusAlias(models.Model):
+    campus = models.ForeignKey(Campus, on_delete=models.CASCADE, related_name='aliases', null=True, blank=True)
+    alias = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Room Alias: {self.alias}'
+    
+class BuildingAlias(models.Model):
+    building = models.ForeignKey(Building, on_delete=models.CASCADE, related_name='aliases', null=True, blank=True)
+    alias = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Building Alias: {self.alias}'
+
+class RoomAlias(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE, related_name='aliases', null=True, blank=True)
+    alias = models.CharField(max_length=200)
+
+    def __str__(self):
+        return f'Campus Alias: {self.alias}'
+
+
+
